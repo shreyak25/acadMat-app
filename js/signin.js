3e1da3eb0e9424b1056ff4b0b4b45935c2eeafaa
shreyak25 <shreyakumawat25@gmail.com>
2022-03-08 23:06:57 +0530

    change

---
+++ js/signin.js
@@ -0,0 +1,19 @@
+// function validateForm() {
+//     let x = document.forms["signinForm"]["fname"].value;
+//     if (x == "") {
+//         alert("First Name must be entered");
+//         return false;
+//     }
+
+//     let x = document.forms["signinForm"]["email"].value;
+//     if (x == "") {
+//         alert("Email must be entered");
+//         return false;
+//     }
+
+//     let x = document.forms["signinForm"]["pass"].value;
+//     if (x == "") {
+//         alert("Password not entered");
+//         return false;
+//     }
+// }
